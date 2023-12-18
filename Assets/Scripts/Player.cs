e84f97137efe605ad0a96e96b36786fab86d9f92
N41LL1W <willian.play.cel@gmail.com>
2023-12-18 20:31:02 -0300

    Player Bounds

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -22,7 +22,26 @@ public class Player : MonoBehaviour
     {
         float horizontalInput = Input.GetAxis("Horizontal");
         float vertivcalInput = Input.GetAxis("Vertical");
+
         transform.Translate(Vector3.right * speed * horizontalInput * Time.deltaTime);
         transform.Translate(Vector3.up * speed * vertivcalInput * Time.deltaTime);
+
+        if (transform.position.y > 4.2f)
+        {
+            transform.position = new Vector3(transform.position.x, 4.2f, 0);
+        }
+        else if (transform.position.y < -4.2f)
+        {
+            transform.position = new Vector3(transform.position.x, -4.2f, 0);
+        }
+        else if (transform.position.x > 8.4f)
+        {
+            transform.position = new Vector3(8.4f, transform.position.y, 0);
+        }
+        else if (transform.position.x < -8.4f)
+        {
+            transform.position = new Vector3(-8.4f, transform.position.y, 0);
+        }
+
     }
 }
