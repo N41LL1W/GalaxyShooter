32b6b574ad0fd346ab51e0894895219c0d386d94
N41LL1W <willian.play.cel@gmail.com>
2023-12-20 09:22:33 -0300

    Created lazer prefab

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,7 +8,9 @@ public class Player : MonoBehaviour
     //data type ( int, float, bool, string )
     //every variable has a NAME
     //option value assigned
-    public float speed = 5.0f;
+
+    [SerializeField]
+    private float speed = 5.0f;
 
     // Start is called before the first frame update
     void Start()
@@ -20,28 +22,40 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Movement();
+    }
+
+    private void Movement()
+    {
         float horizontalInput = Input.GetAxis("Horizontal");
         float vertivcalInput = Input.GetAxis("Vertical");
 
         transform.Translate(Vector3.right * speed * horizontalInput * Time.deltaTime);
         transform.Translate(Vector3.up * speed * vertivcalInput * Time.deltaTime);
 
-        if (transform.position.y > 4.2f)
+
+        //if player on the y is greater than 0
+        // set player position on the Y to 0
+
+        if (transform.position.y > 0)
         {
-            transform.position = new Vector3(transform.position.x, 4.2f, 0);
+            transform.position = new Vector3(transform.position.x, 0, 0);
         }
         else if (transform.position.y < -4.2f)
         {
             transform.position = new Vector3(transform.position.x, -4.2f, 0);
         }
-        else if (transform.position.x > 8.4f)
-        {
-            transform.position = new Vector3(8.4f, transform.position.y, 0);
-        }
-        else if (transform.position.x < -8.4f)
-        {
-            transform.position = new Vector3(-8.4f, transform.position.y, 0);
-        }
 
+        //if player position on the x is greater than 9.5
+        //position on the x needs to be -9.5
+        if (transform.position.x > 9.5f)
+        {
+            transform.position = new Vector3(-9.5f, transform.position.y, 0);
+        }
+        else if (transform.position.x < -9.5f)
+        {
+            transform.position = new Vector3(9.5f, transform.position.y, 0);
+        }
     }
+
 }
